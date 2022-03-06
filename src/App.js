180ff8b68cf0983f8c2ca42ab021bb46e108863f
cobrafinance.contact <cobrafinance.contact@gmail.com>
2022-03-05 20:14:32 -0500

    basic caver setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Caver from 'caver-js';
 
@@ -10,20 +9,7 @@ console.log(caver);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save tofgfjhfgrfghfhdfs reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
